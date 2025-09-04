33d53abbf5574da522a546760ef2dc476afb8779
GailPlatt001 <mooneyliam94@hotmail.co.uk>
2025-09-05 01:13:46 +0900

    chore: align Sanity categories with site filters

--- sanity/schemas/tattooImage.js
+++ sanity/schemas/tattooImage.js
@@ -6,17 +6,21 @@ export default {
     { name: 'photo', title: 'Photo', type: 'image', options: { hotspot: true }, validation: (Rule) => Rule.required() },
     { name: 'alt', title: 'Alt Text', type: 'string' },
     {
-      name: 'categories', title: 'Categories', type: 'array', of: [{ type: 'string' }],
-      options: {
-        list: [
-          { title: 'Neo‑Traditional', value: 'neo' },
-          { title: 'Color', value: 'color' },
-          { title: 'Black & Grey', value: 'black' },
-          { title: 'Lettering', value: 'lettering' },
-        ],
-        layout: 'tags',
-      },
-    },
+  name: 'categories',
+  title: 'Categories',
+  type: 'array',
+  of: [{ type: 'string' }],
+  options: {
+    list: [
+      { title: 'Neo-Traditional', value: 'Neo-Traditional' },
+      { title: 'Black & Grey', value: 'Black & Grey' },
+      { title: 'Delicate', value: 'Delicate' },
+      { title: 'Small Tattoos', value: 'Small Tattoos' },
+      { title: 'Other', value: 'Other' },
+    ],
+    layout: 'tags',
+  },
+},
     { name: 'featured', title: 'Featured on Home', type: 'boolean', initialValue: false },
     { name: 'order', title: 'Display Order', type: 'number', description: 'Lower shows first.' },
   ],
