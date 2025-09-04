6e726f377ed5e2be26fcb8a8f6e60fcaa0eddd69
GailPlatt001 <mooneyliam94@hotmail.co.uk>
2025-09-04 22:08:04 +0900

    Create Config.js

---
+++ components/Config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  IG_HANDLE: "august_tattooer",
+  BRAND_WATERMARK: "AugustTattoo",
+  WHATSAPP_NUMBER: process.env.NEXT_PUBLIC_WHATSAPP || "",
+  DEFAULT_UTM: "?utm_source=site&utm_medium=cta&utm_campaign=portfolio",
+};
+export default CONFIG;
